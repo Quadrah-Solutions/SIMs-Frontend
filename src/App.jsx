c2325d3e333eca2b566e91e70e98865f5e9207fd
Yaw_Boateng <asantea176@gmail.com>
2025-10-11 05:39:29 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
